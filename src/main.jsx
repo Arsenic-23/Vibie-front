44a330652d62969c7c66eb7e35e3ccd3d7e74300
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-13 00:22:00 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
+import { UIProvider } from './context/UIContext'; // Import the UIProvider
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <UIProvider> {/* Wrap the app with the UIProvider to manage UI state */}
+      <ThemeProvider> {/* Wrap the app with ThemeProvider for theme context */}
+        <BrowserRouter> {/* Wrap the app with BrowserRouter for routing */}
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </UIProvider>
   </React.StrictMode>
 );

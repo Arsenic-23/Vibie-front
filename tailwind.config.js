8539825bcf17628b75f8a4b7dfe9c174d97f3e38
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-12 15:47:36 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,11 +16,16 @@ export default {
       },
       animation: {
         spinSlow: 'spinSlow 20s linear infinite',
+        popupIsland: 'popupIsland 0.3s ease-out forwards',
       },
       keyframes: {
         spinSlow: {
           '0%': { transform: 'rotate(0deg)' },
           '100%': { transform: 'rotate(360deg)' },
+        },
+        popupIsland: {
+          '0%': { transform: 'translate(-50%, 20%) scale(0.8)', opacity: '0' },
+          '100%': { transform: 'translate(-50%, 0%) scale(1)', opacity: '1' },
         },
       },
       perspective: {

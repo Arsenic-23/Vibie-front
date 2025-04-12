503000dc622a25d44f546e07b353fcd97761f725
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-12 20:18:22 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,11 +16,17 @@ export default {
       },
       animation: {
         spinSlow: 'spinSlow 20s linear infinite',
+        pop: 'pop 0.3s ease-in-out', // Heart animation
       },
       keyframes: {
         spinSlow: {
           '0%': { transform: 'rotate(0deg)' },
           '100%': { transform: 'rotate(360deg)' },
+        },
+        pop: {
+          '0%': { transform: 'scale(1)', opacity: 1 },
+          '50%': { transform: 'scale(1.4)', opacity: 0.8 },
+          '100%': { transform: 'scale(1)', opacity: 1 },
         },
       },
       perspective: {

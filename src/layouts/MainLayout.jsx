b2fbceb9977316f03c416d7f27517386951e8189
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-13 00:19:35 +0530

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,16 @@
+// src/layouts/MainLayout.jsx
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavigationBar from '../components/NavigationBar';
+import { useUIContext } from '../context/UIContext';
 
 export default function MainLayout() {
+  const { isSongQueueOpen } = useUIContext();
+
   return (
     <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white transition-all duration-500 pb-20">
       <Outlet />
-      <NavigationBar />
+      {!isSongQueueOpen && <NavigationBar />} {/* Only show the NavigationBar when the song queue is not open */}
     </div>
   );
 }

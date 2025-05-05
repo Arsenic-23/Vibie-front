89ba688d5ecb6217ac0ce3861ccda8f1bdb894b1
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-05-05 10:26:13 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
-import { UIProvider } from './context/UIContext'; // Import the UIProvider
+import { UIProvider } from './context/UIContext';
+import { Toaster } from 'react-hot-toast'; // Import Toaster
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UIProvider> {/* Wrap the app with the UIProvider to manage UI state */}
-      <ThemeProvider> {/* Wrap the app with ThemeProvider for theme context */}
-        <BrowserRouter> {/* Wrap the app with BrowserRouter for routing */}
+    <UIProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          {/* Add Toaster to display toast notifications globally */}
+          <Toaster position="top-center" reverseOrder={false} />
           <App />
         </BrowserRouter>
       </ThemeProvider>

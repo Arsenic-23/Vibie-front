d4dc6d1fb92297f36fd5183db891368baa49c173
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-05-11 16:57:26 +0530

    Update Search.jsx

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -111,7 +111,7 @@ export default function Search() {
       if (window.SiriWave && container && !siriWaveRef.current) {
         siriWaveRef.current = new window.SiriWave({
           container,
-          width: 270,
+          width: 200,
           height: 80,
           speed: 0.13,
           amplitude: 2.5,

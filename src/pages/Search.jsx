8b82f5cc27e43749c4e511415f74aca9c4a2f55c
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-10-10 15:32:30 +0530

    Update Search.jsx

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -37,7 +37,7 @@ export default function Search() {
       if (!query) return;
       setLoading(true);
       try {
-        const res = await axios.get('https://attract-behavior-lang-korean.trycloudflare.com/search/', {
+        const res = await axios.get('https://eval-focal-hints-care.trycloudflare.com/search/', {
           params: { q: query, page },
         });
         const newResults = res.data.results || [];

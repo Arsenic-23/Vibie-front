4a2cb4052b2061561e1fd361543984f2dce5c8bf
utkarsh <156888342+Arsenic-23@users.noreply.github.com>
2025-12-03 01:52:47 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+// src/main.jsx 
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -5,16 +7,18 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import { UIProvider } from './context/UIContext';
-import { Toaster } from 'react-hot-toast'; // Import Toaster
+import { Toaster } from 'react-hot-toast';
+import { RealtimeProvider } from './context/RealtimeContext';   
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UIProvider>
       <ThemeProvider>
         <BrowserRouter>
-          {/* Add Toaster to display toast notifications globally */}
-          <Toaster position="top-center" reverseOrder={false} />
-          <App />
+          <RealtimeProvider>   {/* ✅ WRAP APP INSIDE THIS */}
+            <Toaster position="top-center" reverseOrder={false} />
+            <App />
+          </RealtimeProvider>  {/* ✅ */}
         </BrowserRouter>
       </ThemeProvider>
     </UIProvider>
